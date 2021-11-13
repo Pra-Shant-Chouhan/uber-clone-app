04a35c82c84d8fbb34f3968ac0698379e297e4b0
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-11-13 19:08:33 +0530

    Add car-list api data

---
+++ pages/data/Car-list.js
@@ -0,0 +1,37 @@
+export const carList = [
+    {
+        imgUrl: 'https://i.ibb.co/xLrsLCg/Alto-800-Car-png.png',
+        CarName: 'Maruti Suziki Alto',
+        Time: "5 min away",
+        Rent: "2500 Rs.",
+        multiplier: 1,
+    },
+    {
+        imgUrl: "https://i.ibb.co/ZxtHV4G/Wagon-R-Car-Png.png" ,
+        CarName: 'Wagonor',
+        Time: "7 min away",
+        Rent: "3500 Rs.",
+        multiplier: 1.5,
+    },
+    {
+        imgUrl: "https://i.ibb.co/8xP0ptF/Toyota-Car-Free-PNG-Image.png",
+        CarName: 'CARMY',
+        Time: "8 min away",
+        Rent: "5000 Rs.",
+        multiplier: 2,
+    },
+    {
+        imgUrl: "https://i.ibb.co/8gfq0n7/Downloaded-from-Oye-Be-Smartest-11569567899bzjd1mpidk33qwln9cv2jznwqnchimgboghxvcexmqmzxgi0xqynw5m92.png",
+        CarName: 'BMW',
+        Time: " 10 min away",
+        Rent: "10000 Rs.",
+        multiplier: 1.2,
+    },
+    {
+        imgUrl: "https://i.ibb.co/QmHH2s4/Car-Download-PNG.png",
+        CarName: 'Toyota',
+        Time: "2 min away",
+        Rent: "7000 Rs.",
+        multiplier: 2.8,
+    }
+]
