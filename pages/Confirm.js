8b24e77ffc546b82117c69a80f439d1d6fd94d9c
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-12-17 20:07:52 +0530

    edit for buld file structer change page folder do not contain any folder for build

--- pages/Confirm.js
+++ pages/Confirm.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import tw from "tailwind-styled-components"
-import Map from './component/map'
+import Map from '../component/map'
 import { useRouter } from 'next/router'
-import RideSelector from './component/RideSelector'
+import RideSelector from '../component/RideSelector'
 import Link from "next/link"
 
 
